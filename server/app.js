25c12989c2a673221da83df98a6865b7f222ccda
rizzhal <barmanbishal1234@gmail.com>
2026-07-19 23:16:32 +0530

    configured cors for deployment

--- server/app.js
+++ server/app.js
@@ -3,7 +3,13 @@ import cors from "cors"
 import resumeRoutes from "./routes/resumeRoute.js"
 const app = express();
 
-app.use(cors())
+app.use(cors({
+    origin:[
+        "http://localhost:5173",
+        "https://hackathon1-rouge-ten.vercel.app/"
+    ],
+    credentials: true
+}))
 app.use(express.json());
 
 app.use("/api/resume" , resumeRoutes);
